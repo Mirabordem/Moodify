cb1987b2032de02068fe6c6b954bdf96b7db9712
Alex Heasley <alex.heasley@gmail.com>
2023-10-21 18:26:02 -0500

    fda

--- react-app/src/components/AlbumDetails/index.js
+++ react-app/src/components/AlbumDetails/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useHistory, NavLink, useParams } from "react-router-dom";
-import OpenModalButton from "../../OpenModalButton/index";
+import OpenModalButton from "../OpenModalButton/index";
 import { useModal } from '../../context/Modal';
 import "./albumDetails.css"
 
