19810606d9e78be3c2db922a580858562561bf3e
Mirosława Borkowska <130639536+Mirabordem@users.noreply.github.com>
2023-10-21 12:45:27 -0400

    delete .env

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -8,17 +8,40 @@ function Navigation({ isLoaded }){
 	const sessionUser = useSelector(state => state.session.user);
 
 	return (
-		<ul>
-			<li>
-				<NavLink exact to="/">Home</NavLink>
-			</li>
-			{isLoaded && (
-				<li>
-					<ProfileButton user={sessionUser} />
-				</li>
-			)}
-		</ul>
-	);
-}
+		<div className='nav-container'>
+			<NavLink exact to="/">
+				<img className="Moodify-logo" alt="" src=''></img>
+			</NavLink>
+		  {isLoaded && (
+			<div className='nav-profile'>
+				{sessionUser ? (
+					<NavLink className='new-album' to='/albums/new'>
+						Create Album
+					</NavLink>
+				) : null}
+			  <ProfileButton user={sessionUser} />
+			  </div>
+		  )}
+		  </div>
+	  );
+	}
 
-export default Navigation;
+	export default Navigation;
+
+
+
+// 	return (
+// 		<ul>
+// 			<li>
+// 				<NavLink exact to="/">Home</NavLink>
+// 			</li>
+// 			{isLoaded && (
+// 				<li>
+// 					<ProfileButton user={sessionUser} />
+// 				</li>
+// 			)}
+// 		</ul>
+// 	);
+// }
+
+// export default Navigation;
