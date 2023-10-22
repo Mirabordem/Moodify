2e48224f0c2424856d8154ea511709b4706c448a
Josh Goldenberg <129535504+jgoldenberg29@users.noreply.github.com>
2023-10-22 15:01:22 -0400

    home page card lists coded, react routes paired back for modal consideration, small changes to flask album routes

--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -7,19 +7,85 @@ import "./home.css"
 
 export default function Home() {
 let albumId=1
-//get rid of above value when we start dynamic mapping
+// //get rid of above value when we start dynamic mapping
+const playlistIds = useSelector(state => state.user.userPlaylists)
+const albums = useSelector(state => state.albums)
+const playlists = useSelector(state => state.playlists)
+
+const albumsArray = Object.values(albums)
+const albumsSample = []
+for (let i = 0; i <= 4; i++) {
+    albumsSample.push(albumsArray[i])
+}
+
+const playlistsSample = []
+for (let id of playlistIds) {
+    if (playlistsSample.length <= 4) playlistsSample.push(playlists[id])
+}
+
+const albumCards = albumsSample.map(album =>{
+    return (
+        <NavLink
+        className="card sample-card album-card"
+        key={album.id}
+        exact to={`albums/${album.id}`}
+        >
+            <div className="card-image-container">
+                <img src={album.coverImageUrl}/>
+            </div>
+            <div className="card-info-container">
+                <p>{album.title}</p>
+                <p>{album.artist}</p>
+            </div>
+        </NavLink>
+    )
+});
+
+const playlistCards = playlistsSample.map(playlist =>{
+    return (
+        <NavLink
+        className="card sample-card playlists-card"
+        key={playlist.id}
+        exact to={`playlistss/${playlist.id}`}
+        >
+            <div className="card-image-container">
+                <img src={playlist.coverImageUrl}/>
+            </div>
+            <div className="card-info-container">
+                <p>{playlist.name}</p>
+                {/* playlist description preview? */}
+                {/* number of tracks? */}
+            </div>
+        </NavLink>
+    )
+});
+
+
+
 
 return (
 
     <div className='main_window_container'>
 
-    <h1 className='homeText'>THIS IS THE HOME COMPONENT</h1>
-
-    <div className='link-container'>
-
-    <Link to={`/albums/${albumId}`}>im an album click me</Link>
-    </div>
-
+        <h1 className='homeText'>Welcome to Moodify!</h1>
+        <div className='card-container cample-container'>
+            <div className="cards-list-title">
+                <h3>Albums</h3>
+                <NavLink exact to='/albums'>All albums</NavLink>
+            </div>
+            <div className='card-map-div'>
+                {albumCards}
+            </div>
+        </div>
+        <div className='card-container sample-container'>
+        <div className="cards-list-title">
+                <h3>My Playlists</h3>
+                <NavLink exact to='/playlists'>All My Playlists</NavLink>
+            </div>
+            <div className='card-map-div'>
+              {playlistCards}
+            </div>
+        </div>
     </div>
 
 
