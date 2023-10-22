ae6e05b1125680c0f9a45010bc82bb3cf4f09ca5
Alex Heasley <alex.heasley@gmail.com>
2023-10-21 21:22:46 -0500

    set up ok now lets pull down from dev

--- react-app/src/components/SideMenu/index.js
+++ react-app/src/components/SideMenu/index.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useHistory, NavLink, useParams } from "react-router-dom";
-import OpenModalButton from "../../OpenModalButton/index";
-import { useModal } from '../../context/Modal';
+import OpenModalButton from "../OpenModalButton/index";
+import { useModal } from "../../context/Modal";
 import "./sideMenu.css"
 
 
