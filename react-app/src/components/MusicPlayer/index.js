74995b3acbdb083f64e789be4d2ea332021c364a
Hayden Gogan <h.gogan03@icloud.com>
2023-10-22 09:00:37 -0700

    Added songList component

--- react-app/src/components/MusicPlayer/index.js
+++ react-app/src/components/MusicPlayer/index.js
@@ -2,20 +2,23 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useHistory, NavLink, useParams } from "react-router-dom";
 import OpenModalButton from "../OpenModalButton/index";
-import { useModal } from '../../context/Modal';
-import './MusicPlayer.css';
+import { useModal } from "../../context/Modal";
+import "./MusicPlayer.css";
 
-
-
-
+/*
+what do we need?
+- way for music player to know what song to play including all info
+- way for music player to know and play next song when song ends or button is hit
+- list of songs: playlist or album, current song, next song, song progress, previous song
+- functionality: play, pause, next, restart, slider bar, prev song, volume?, like?
+- isPlaying, nextSong, currentSong, prevSong , songLength, currentPercent
+We need context for isPlaying, nextSong, currentSong, prevSong other two are calculated from song
+*/
 
 export default function MusicPlayer() {
-
-
-    return (
-        <div className='musicPlayer'>
-        <h1>THIS IS THE MUSICPLAYER COMPONENT</h1>
-        </div>
-    )
-
-    }
+  return (
+    <div className="musicPlayer">
+      <h1>THIS IS THE MUSICPLAYER COMPONENT</h1>
+    </div>
+  );
+}
---
+++ react-app/src/components/SongList/index.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useHistory, NavLink, useParams } from "react-router-dom";
+import OpenModalButton from "../OpenModalButton/index";
+import { useModal } from "../../context/Modal";
+import { thunkGetAllAlbums } from "../../store/albums";
+import { getAllAlbums } from "../../store/albums";
+import { getAllPlaylists } from "../../store/playlists";
+import { getAllSongs } from "../../store/songs";
+import fetchAll from "../utils";
+
+export default function SongList({ songs, artist }) {
+  const songList = songs.map((song) => {
+    const minutes = Math.trunc(song.songLength / 60);
+    const seconds = song.songLength % 60;
+    const runTime = `${minutes}:${seconds}`;
+    return (
+      <li className="song-li" key={song.id}>
+        <span className="song-info">{song.trackNumber}</span>
+        <span className="song-info">{song.name}</span>
+        <span className="song-info">{artist}</span>
+        <span className="song-info">
+          <i class="fa-regular fa-heart"></i>
+        </span>
+        <span className="song-info">{runTime}</span>
+        <span>
+          <button className="song-menu">
+            <i class="fa-solid fa-ellipsis"></i>
+          </button>
+        </span>
+      </li>
+    );
+  });
+
+  return (
+    <div id="album-id-song-list">
+      <ul>{songList}</ul>
+    </div>
+  );
+}
