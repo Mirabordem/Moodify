ab0c1bf82266b2efc72c77faa91448a4271d1675
Josh Goldenberg <129535504+jgoldenberg29@users.noreply.github.com>
2023-10-27 11:53:11 -0400

    removed unused imports

--- react-app/src/components/ProfilePage/index.js
+++ react-app/src/components/ProfilePage/index.js
@@ -1,6 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link, useHistory, NavLink, useParams } from "react-router-dom";
+import React from "react";
 import "./ProfilePage.css"
 
 
--- react-app/src/components/utils.js
+++ react-app/src/components/utils.js
@@ -1,8 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { getAllAlbums } from "../store/albums";
-import { getAllPlaylists } from "../store/playlists";
-import { getAllSongs } from "../store/songs";
+
 
 const fetchAll = (dispatch, getAllAlbums, getAllPlaylists, getAllSongs) => {
   fetch("/api/all")
