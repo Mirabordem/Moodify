aa11143ba367854acf11bc79a429856abf2151d5
Mirosława Borkowska <130639536+Mirabordem@users.noreply.github.com>
2023-10-27 20:42:24 -0400

    footer still

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -1,5 +1,3 @@
-
-
 import "./Footer.css";
 
 export default function Footer() {
@@ -9,27 +7,48 @@ export default function Footer() {
         <div className="creators1">Created by:</div>
         <div className="creator">
           <div className="creator-text">Miroslawa (Mira) Borkowska</div>
-          <a href="https://github.com/Mirabordem" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://github.com/Mirabordem"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <i className="fab fa-github creator-link-icon"></i>
           </a>
+          <div className="dot">•</div>
         </div>
         <div className="creator">
           <div className="creator-text">Hayden Gogan</div>
-          <a href="https://github.com/h-moon-g" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://github.com/h-moon-g"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <i className="fab fa-github creator-link-icon"></i>
           </a>
+          <div className="dot">•</div>
         </div>
         <div className="creator">
           <div className="creator-text">Josh Goldenberg</div>
-          <a href="https://github.com/jgoldenberg29" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://github.com/jgoldenberg29"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <i className="fab fa-github creator-link-icon"></i>
           </a>
+          <div className="dot">•</div>
         </div>
         <div className="creator">
           <div className="creator-text">Alex Heasley</div>
-          <a href="https://github.com/alexchrono" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://github.com/alexchrono"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <i className="fab fa-github creator-link-icon"></i>
           </a>
+          <div className="dot">•</div>
+          <div className="year-aa">October 2023</div>
         </div>
       </div>
     </div>
