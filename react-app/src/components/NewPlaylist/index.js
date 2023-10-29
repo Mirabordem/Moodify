727e1c78b5fdad27238d2457c6777040f449090d
Alex Heasley <alex.heasley@gmail.com>
2023-10-29 14:08:59 -0500

    before i mess a second th ing up

--- react-app/src/components/NewPlaylist/index.js
+++ react-app/src/components/NewPlaylist/index.js
@@ -139,6 +139,9 @@ return (
                 </label> */}
                 {errors.cover_image_url && <p className="add-playlist-errors">{errors.cover_image_url}</p>}
                 <button className="signup-button" type="submit">{formType === 'Create' ? 'Create Playlist' : 'Edit Playlist'}</button>
+                <button type="button" className="signup-button" onClick={closeModal}>
+                    Cancel
+                </button>
             </form>
         </div>
     )
