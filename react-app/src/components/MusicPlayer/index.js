4afb6f8db1508efb25f5a652acd0f38862ac492d
Josh Goldenberg <129535504+jgoldenberg29@users.noreply.github.com>
2023-11-13 17:51:08 -0500

    music player shuffles automatically and plays all songs from beginning of shuffled songs when you click play with no song loaded.

--- react-app/src/components/MusicPlayer/index.js
+++ react-app/src/components/MusicPlayer/index.js
@@ -15,9 +15,9 @@ We need context for isPlaying, nextSong, currentSong, prevSong other two are cal
 */
 
 export default function MusicPlayer() {
-  //   const [skipped, setSkipped] = useState(false);
+
   const audio = useRef();
-  // const user = useSelector(state => state.session.user)
+  const allSongs = useSelector(state => state.songs)
   const [songProgress, setSongProgress] = useState();
   const {
     isPlaying,
@@ -36,6 +36,8 @@ export default function MusicPlayer() {
     setCurrentSongIndex,
     currentVolume,
     setVolume,
+    shuffle,
+    setShuffle
   } = useSongPlayer();
 
   useEffect(() => {
@@ -72,12 +74,6 @@ export default function MusicPlayer() {
       setPrevSong(songQueue[currentSongIndex]);
       setCurrentSong(songQueue[currentSongIndex + 1]);
       setCurrentSongIndex(currentSongIndex + 1);
-      // if (currentSongIndex !== songQueue.length - 1) {
-      //   setNextSong(songQueue[currentSongIndex + 1]);
-      // }
-      // if (!prevSong.name) {
-      //   setPrevSong()
-      // }
     }
     if (isPlaying === true) {
       setPlayAnyway(true);
@@ -90,9 +86,6 @@ export default function MusicPlayer() {
       setNextSong(songQueue[currentSongIndex]);
       setCurrentSong(songQueue[currentSongIndex - 1]);
       setCurrentSongIndex(currentSongIndex - 1);
-      // if (currentSongIndex !== 0) {
-      //   setPrevSong(songQueue[currentSongIndex - 1]);
-      // }
     }
     if (isPlaying === true) {
       setPlayAnyway(true);
@@ -100,6 +93,20 @@ export default function MusicPlayer() {
     setIsPlaying(true);
   };
 
+  const randomNum = (list) => {
+    console.log("🚀 ~ file: index.js:97 ~ randomNum ~ list:", list)
+    console.log("🚀 ~ file: index.js:97 ~ randomNum ~ list:", list.length)
+
+    const randomNum = Math.floor(Math.random() * list.length)
+    console.log("🚀 ~ file: index.js:98 ~ randomNum ~ randomNum:", randomNum)
+    return randomNum
+  }
+
+  const shuffleSongs = (songsArr) => {
+   return songsArr.sort(() => Math.random() - 0.5)
+  }
+
+  console.log("currentSong", currentSong)
   return (
     <div className="musicPlayer">
       <audio
@@ -116,18 +123,35 @@ export default function MusicPlayer() {
       </div>
 
       <div className="controls">
-        <button onClick={() => playPrev()}>Previous</button>
+        <button
+        onClick={() => playPrev()}
+        disabled={!currentSong?.name}
+        >
+          Previous</button>
         <div className="dot5">•</div>
         <button
           onClick={() => {
-            if (songQueue?.length && currentSong?.name)
+            if (songQueue?.length && currentSong?.name) {
               setIsPlaying(!isPlaying);
+            }
+            else {
+              const newQueue = shuffleSongs(Object.values(allSongs))
+              setSongQueue(newQueue)
+              setCurrentSong(newQueue[0])
+              setNextSong(newQueue[1])
+              setIsPlaying(true)
+            }
           }}
+          // disabled={!currentSong?.name}
         >
           {!isPlaying ? "Play" : "Pause"}
         </button>
         <div className="dot5">•</div>
-        <button onClick={() => playNext()}>Next</button>
+        <button
+        onClick={() => playNext()}
+        disabled={!currentSong?.name}
+        >
+          Next</button>
       </div>
 
       <div className="progress-bar">
@@ -146,131 +170,3 @@ export default function MusicPlayer() {
     </div>
   );
 }
-
-// import React, { useEffect, useRef, useState } from "react";
-// import ReactSlider from "react-slider";
-// import { useSongPlayer } from "../../context/SongPlayer";
-// import { useSelector } from "react-redux";
-
-// /*
-// what do we need?
-// - way for music player to know what song to play including all info
-// - way for music player to know and play next song when song ends or button is hit
-// - list of songs: playlist or album, current song, next song, song progress, previous song
-// - functionality: play, pause, next, restart, slider bar, prev song, volume?, like?
-// - isPlaying, nextSong, currentSong, prevSong , songLength, currentPercent
-// We need context for isPlaying, nextSong, currentSong, prevSong other two are calculated from song
-// */
-
-// export default function MusicPlayer() {
-//   //   const [skipped, setSkipped] = useState(false);
-//   const audio = useRef();
-//   // const user = useSelector(state => state.session.user)
-//   const [songProgress, setSongProgress] = useState();
-//   const {
-//     isPlaying,
-//     setIsPlaying,
-//     nextSong,
-//     setNextSong,
-//     currentSong,
-//     setCurrentSong,
-//     prevSong,
-//     setPrevSong,
-//     songQueue,
-//     setSongQueue,
-//     playAnyway,
-//     setPlayAnyway,
-//     currentSongIndex,
-//     setCurrentSongIndex,
-//   } = useSongPlayer();
-
-//   useEffect(() => {
-//     if (isPlaying) {
-//       audio.current.play();
-//     } else {
-//       audio.current.pause();
-//     }
-//     if (playAnyway) {
-//       audio.current.play();
-//       setPlayAnyway(false);
-//     }
-//   }, [isPlaying, playAnyway]);
-
-//   const albums = useSelector((state) => state.albums);
-
-//   //   useEffect(() => {
-//   //     if (isPlaying && user) {
-//   //       audio.current.play();
-//   //     } else if (!isPlaying && user) {
-//   //       audio.current.pause();
-//   //     } else if (isPlaying && !user) {
-//   //       alert("Sign in to play songs")
-//   //     }
-//   //     if (playAnyway && user) {
-//   //       audio.current.play();
-//   //       setPlayAnyway(false);
-//   //     }
-
-//   // }, [isPlaying, playAnyway, user]);
-
-//   const playNext = () => {
-//     console.log('prevSong', prevSong)
-//     console.log('currentSong', currentSong)
-//     console.log('nextSong', nextSong)
-//     if (nextSong) {
-//       setPrevSong(songQueue[currentSongIndex]);
-//       setCurrentSong(songQueue[currentSongIndex + 1]);
-//       setCurrentSongIndex(currentSongIndex + 1);
-//       if (currentSongIndex !== songQueue.length - 1) {
-//         setNextSong(songQueue[currentSongIndex + 1]);
-//       }
-//       // if (!prevSong.name) {
-//       //   setPrevSong()
-//       // }
-//     }
-//     if (isPlaying === true) {
-//       setPlayAnyway(true);
-//     }
-//     setIsPlaying(true);
-//   };
-
-//   const playPrev = () => {
-//     console.log('prevSong', prevSong)
-//     console.log('currentSong', currentSong)
-//     console.log('nextSong', nextSong)
-//     if (prevSong) {
-//       setNextSong(songQueue[currentSongIndex]);
-//       setCurrentSong(songQueue[currentSongIndex - 1]);
-//       setCurrentSongIndex(currentSongIndex - 1);
-//       if (currentSongIndex !== 0) {
-//         setPrevSong(songQueue[currentSongIndex - 1]);
-//       }
-//     }
-//     if (isPlaying === true) {
-//       setPlayAnyway(true);
-//     }
-//     setIsPlaying(true);
-//   };
-
-//   return (
-//     <div className="musicPlayer">
-//       <audio
-//         src={currentSong.audioUrl}
-//         ref={audio}
-//         onEnded={() => playNext()}
-//       />
-//       <div>
-//         <p>{currentSong?.name}</p>
-//         <p>{albums[currentSong?.albumId]?.artist}</p>
-//       </div>
-//       <span onClick={() => playPrev()}>Prev</span>
-//       <button onClick={() =>  {
-//         if(songQueue.length && currentSong.name) setIsPlaying(!isPlaying)
-//       }}>
-//         {!isPlaying ? "Play" : "Pause"}
-//       </button>
-//       <span onClick={() => playNext()}>Next</span>
-//       <ReactSlider />
-//     </div>
-//   );
-// }
