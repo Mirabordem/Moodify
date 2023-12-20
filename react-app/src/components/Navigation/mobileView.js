5b8458f41cbb73c6f3659730e3f5d55ff5f9f893
Alex Heasley <alex.heasley@gmail.com>
2023-12-19 19:44:09 -0600

    everything almost perfect lets toggle musicplayer view with the javvascript

--- react-app/src/components/Navigation/mobileView.js
+++ react-app/src/components/Navigation/mobileView.js
@@ -8,6 +8,7 @@ const toggleSideMenu = () => {
       const currentDisplay = sideToggle.style.display;
       sideMenu.style.width = currentWidth === '60%' ? '0%' : '60%'; // Toggle between 0% and 60%
       sideToggle.style.display = currentDisplay ==='inline-block'? 'none' : 'inline-block'
+      
     }
   };
 
