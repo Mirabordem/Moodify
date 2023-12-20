231f165a7bb781f5b3e19ffadcba0a439d0bbcf3
Alex Heasley <alex.heasley@gmail.com>
2023-12-19 20:02:26 -0600

    even more progress with playlist view

--- react-app/src/components/Navigation/mobileView.js
+++ react-app/src/components/Navigation/mobileView.js
@@ -1,17 +1,22 @@
 const toggleSideMenu = () => {
-    const sideMenu = document.querySelector('.side-menu');
-    const sideToggle=  document.querySelector('.sideToggleAlex');
+  const sideMenu = document.querySelector('.side-menu');
+  const sideToggle = document.querySelector('.sideToggleAlex');
+  const musicPlayer = document.querySelector('.musicPlayer');
 
-    console.log('sideMenu is ',sideMenu)
-    if (sideMenu) {
+  console.log('sideMenu is ', sideMenu);
+  if (sideMenu) {
       const currentWidth = sideMenu.style.width;
       const currentDisplay = sideToggle.style.display;
-      sideMenu.style.width = currentWidth === '60%' ? '0%' : '60%'; // Toggle between 0% and 60%
-      sideToggle.style.display = currentDisplay ==='inline-block'? 'none' : 'inline-block'
-      
-    }
-  };
 
+      sideMenu.style.width = currentWidth === '60%' ? '0%' : '60%';
+      sideToggle.style.display = currentDisplay === 'inline-block' ? 'none' : 'inline-block';
 
+      if (sideMenu.style.width === '60%' && sideToggle.style.display === 'inline-block') {
+          musicPlayer.style.display = 'none';
+      } else {
+          musicPlayer.style.display = 'block';
+      }
+  }
+};
 
-  export default toggleSideMenu;
+export default toggleSideMenu;
